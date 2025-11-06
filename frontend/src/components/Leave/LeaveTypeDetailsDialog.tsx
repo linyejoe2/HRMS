11c4531e42d7d9a23f12cd7f901a4858242976c3
Randy <linyejoe2@gmail.com>
2025-11-06 11:47:39 +0800

    tweak: Add leave description in LeaveTypeDetailsDialog

--- frontend/src/components/Leave/LeaveTypeDetailsDialog.tsx
+++ frontend/src/components/Leave/LeaveTypeDetailsDialog.tsx
@@ -25,6 +25,32 @@ interface LeaveTypeDetailsDialogProps {
   leaves: LeaveRequest[];
 }
 
+const getLeaveRule = (leaveType: string): string => {
+  switch (leaveType) {
+    case "事假":
+      return "勞工請假規則第7條：「勞工因有事故必須親自處理者，得請事假，一年內合計不得超過十四日。事假期間不給工資。」"
+    case "普通傷病假":
+      return `勞工請假規則第4條：「
+勞工因普通傷害、疾病或生理原因必須治療或休養者，得在左列規定範圍內請普通傷病假：
+一、未住院者，一年內合計不得超過三十日。
+二、住院者，二年內合計不得超過一年。
+三、未住院傷病假與住院傷病假二年內合計不得超過一年。
+經醫師診斷，罹患癌症（含原位癌）採門診方式治療或懷孕期間需安胎休養者，其治療或休養期間，併入住院傷病假計算。
+普通傷病假一年內未超過三十日部分，工資折半發給，其領有勞工保險普通傷病給付未達工資半數者，由雇主補足之。」`
+    case "特別休假":
+      return `勞動基準法第38條：「勞工在同一雇主或事業單位，繼續工作滿一定期間者，應依下列規定給予
+特別休假：
+一、六個月以上一年未滿者，三日。
+二、一年以上二年未滿者，七日。
+三、二年以上三年未滿者，十日。
+四、三年以上五年未滿者，每年十四日。
+五、五年以上十年未滿者，每年十五日。
+六、十年以上者，每一年加給一日，加至三十日為止。」`
+    default:
+      return ""
+  }
+}
+
 const LeaveTypeDetailsDialog: React.FC<LeaveTypeDetailsDialogProps> = ({
   open,
   onClose,
@@ -48,6 +74,11 @@ const LeaveTypeDetailsDialog: React.FC<LeaveTypeDetailsDialogProps> = ({
       onClose={onClose}
       maxWidth="md"
       fullWidth
+      PaperProps={{
+        sx: {
+          maxHeight: '80dvh',
+        },
+      }}
     >
       <DialogTitle>
         <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
@@ -63,6 +94,14 @@ const LeaveTypeDetailsDialog: React.FC<LeaveTypeDetailsDialogProps> = ({
       </DialogTitle>
 
       <DialogContent dividers>
+        {/* Descrition Section */}
+
+        <Box sx={{ mb: 3, p: 2, backgroundColor: 'action.hover', borderRadius: 1 }}>
+          <Typography variant="body1" gutterBottom sx={{ whiteSpace: 'pre-line' }}>
+            {getLeaveRule(leaveType)}
+          </Typography>
+        </Box>
+
         {/* Summary Section */}
         <Box sx={{ mb: 3, p: 2, backgroundColor: 'action.hover', borderRadius: 1 }}>
           <Typography variant="subtitle1" fontWeight="bold" gutterBottom>
